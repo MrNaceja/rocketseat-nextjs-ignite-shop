3e86fe176f88e91d1e1f4cdbaa83803555712b8c
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-26 15:18:39 -0300

    feat: setup inicial

---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+export function Button(props: ComponentProps<"button">) {
+    return (
+        <button {...props} className="bg-brand-primary text-white px-4 py-3 font-bold rounded-md hover:bg-brand-light" />
+    )
+}
