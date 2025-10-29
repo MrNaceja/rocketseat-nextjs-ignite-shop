5daef7f494c5b7af3429fbdf84c56714cd53a3a9
Eduardo Toriani <edutoriani13@gmail.com>
2025-10-29 18:48:01 -0300

    feat: ajusta estilos loading page product

--- src/app/product/[id]/loading.tsx
+++ src/app/product/[id]/loading.tsx
@@ -2,8 +2,8 @@ import { LoaderIcon } from "lucide-react"
 
 export default function LoadingProductPage() {
     return (
-        <div>
-            <LoaderIcon className="animate-spin" />
+        <div className="size-full flex flex-col items-center justify-center gap-3 min-h-[75vh]">
+            <LoaderIcon className="animate-spin size-10" />
             <p>Carregando...</p>
         </div>
     )
